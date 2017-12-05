e5fc1f7f78bf267adc01eca1111005e688185ce5
Bartek Styrylski <bartek.styrylski@oracle.com>
2017-12-05 12:55:48 +0100

    Progress

--- src/js/viewModels/tribepad.js
+++ src/js/viewModels/tribepad.js
@@ -7,13 +7,29 @@
 /**
  * tribepad module
  */
-define(['ojs/ojcore', 'knockout'
+define(['ojs/ojcore', 'knockout', 'ojs/ojselectcombobox'
 ], function (oj, ko) {
     /**
      * The view model for the main content view template
      */
     function tribepadContentViewModel() {
         var self = this;
+        
+        self.position = ko.observable();
+        self.positions = ko.observableArray([
+            {value: '300100156412118', label: 'Tesco Store Manager'},
+            {value: '300100156412066', label: 'Tesco Store Worker'},
+            {value: '300100156412096', label: 'Tesco Store Expert'},
+            {value: '300100156412140', label: 'Tesco Region Manager'}
+        ]);
+        
+        self.manager = ko.observable();
+        self.managers = ko.observableArray([
+            {value: '2', label: 'Susan Smith'},
+            {value: '3', label: 'Kenneth Walker'},
+            {value: '4', label: 'Sandra Pierson'},
+            {value: '5', label: 'MaryBeth Richards'}
+        ]);
     }
     
     return tribepadContentViewModel;
